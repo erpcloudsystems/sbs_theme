34acb9b11cdb0713c73b0ecb004d9669323af7a5
Mghoneem10 <mghoneem10@gmail.com>
2024-07-29 18:00:25 +0300

    update sales invoice qrcode

--- sbs_theme/doctype_triggers/accounting/sales_invoice/sales_invoice.js
+++ sbs_theme/doctype_triggers/accounting/sales_invoice/sales_invoice.js
@@ -1,4 +1,6 @@
-/*
-frappe.ui.form.on("Sales Invoice", "validate", function(frm) {
+
+frappe.ui.form.on("Sales Invoice", "custom_update_qr", function(frm) {
+    frappe.call({
+        method:"sbs_theme.doctype_triggers.accounting.sales_invoice.sales_invoice.update_qr_code"
+    })
 });
-*/
