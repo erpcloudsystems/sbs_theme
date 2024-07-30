978cc922856ca1cc2be0b6592145099cab165ddd
Mghoneem10 <mghoneem10@gmail.com>
2024-07-30 10:27:58 +0300

    30-7-2024 10:24 AM

--- sbs_theme/overrides.py
+++ sbs_theme/overrides.py
@@ -8,6 +8,9 @@ from  erpnext.crm.doctype.appointment.appointment import Appointment
 from frappe.utils import get_url, getdate, now
 
 
+from erpnext.crm.doctype.appointment_booking_settings.appointment_booking_settings import AppointmentBookingSettings
+def save(self):
+    super(AppointmentBookingSettings, self).save()
 
 class CustomAppointment(Appointment):
    def send_confirmation_email(self):
--- sbs_theme/scheduler_events/daily.py
+++ sbs_theme/scheduler_events/daily.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from ksa.ksa.saudi_arabia.utils.py import create_qr_code
 
 frappe.whitelist()
 def daily():
-    pass
+    sales_invoices = frappe.get_all("Sales Invoice",{"docstatus":1},["name"])
+    for row in sales_invoices:
+        sales_invoice = frappe.get_doc("Sales Invoice",row["name"])
+        create_qr_code(sales_invoice)    
